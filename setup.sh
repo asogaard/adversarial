3190fcfe62df8a4a6e05db5bf0ff2134617e4965
Andreas Søgaard <andreas.sogaard@gmail.com>
2017-09-01 14:59:31 +0100

    Update setup script to load CUDA

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 # Small script to setup the environment 
 # necessary for running the adversarial 
 # neural network training an evaluation.
-module load anaconda root/6.06.02
+module load anaconda cuda root/6.06.02
 source activate adversarial
