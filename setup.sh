44f122badb56db7ec5fa7a8cdd6ce41a0450745f
Andreas Søgaard <andreas.sogaard@gmail.com>
2017-09-04 13:52:20 +0100

    Update setup script to use separate CPU and GPU environments

--- setup.sh
+++ setup.sh
@@ -2,5 +2,16 @@
 # Small script to setup the environment 
 # necessary for running the adversarial 
 # neural network training an evaluation.
-module load anaconda cuda root/6.06.02
-source activate adversarial
+MODE=$1
+if [ "$MODE" == "cpu" ] || [ "$MODE" == "gpu" ] || [ "$MODE" == "" ]; then
+    if [ "$MODE" == "" ]; then
+	echo "Using cpu by default."
+	MODE="cpu"
+    fi
+    module load anaconda cuda/8.0.61 root/6.06.02
+    source activate adversarial-$MODE
+else
+    echo "Command-line argument '$1' is not either 'cpu' or 'gpu'."
+    return 1
+fi
+return 0
