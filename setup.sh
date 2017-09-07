e75dda821e7f86034dfc9a7169508a625c612035
Andreas Søgaard <andreas.sogaard@gmail.com>
2017-09-07 14:29:19 +0100

    Add flags and unset option; never unload SGE module

--- setup.sh
+++ setup.sh
@@ -3,43 +3,62 @@
 # necessary for running the adversarial 
 # neural network training an evaluation.
 
-# @NOTE: Improve; 'if "test" in args'-type thing?
-MODE=$1
-TEST=$2
-if [ "$MODE" == "test" ]; then
-    MODE=""
-    TEST="test"
-fi
+# Extract flags
+ARGUMENTS=("$@")
+set -- # Unsetting positional arguments, to avoid error from "source deactivate"
+
+CPU=false
+GPU=false
+TEST=false
+UNSET=false
+for ARG in "${ARGUMENTS[@]}"; do
+    if   [ "$ARG" == "cpu" ];  then
+	CPU=true
+    elif [ "$ARG" == "gpu" ];  then
+	GPU=true
+    elif [ "$ARG" == "test" ]; then
+	TEST=true
+    elif [ "$ARG" == "unset" ]; then
+	UNSET=true
+    else
+	echo "Argument '$ARG' was not understood"
+    fi
+done
 
 # Deactivate any current environments
-if [ ! -z "$(python -c "import sys; print sys.version" 2>/dev/null | grep "Continuum\|conda")" ]; then
+# -- Check if command 'conda' exists
+# -- Check whether an environment is activated
+if hash conda 2>/dev/null && [ "$(conda info --envs | grep \* | grep -v root)" ]
+then
     source deactivate
 fi
 
 # Unload any existing modules on Eddie
-LOADED_MODULES="$(module list 2>&1 | grep -v "Currently Loaded" | sed 's/ //g' | sed 's/[0-9]*)/\n/g' | grep -v "^$" | sed 's/\/.*//g')"
-read -r -a LOADED_MODULES <<< $LOADED_MODULES
-for MODULE in "${LOADED_MODULES[@]}"; do
-    module unload $MODULE
-done
+module unload $(module list 2>&1 | tail -n +2 | sed 's/ *[0-9]*) \([a-z]*\)\/[0-9._a-z]*/\1 /g' | sed 's/sge *//g')
 
-# Set up appropriate environment
-if [ "$MODE" == "cpu" ] || [ "$MODE" == "gpu" ] || [ "$MODE" == "" ]; then
-    if [ "$MODE" == "" ]; then
-	echo "Using cpu by default."
-	MODE="cpu"
-    fi
-    module load anaconda cuda/8.0.61 root/6.06.02
-    source activate adversarial-$MODE
-else
-    echo "Command-line argument '$1' is not either 'cpu' or 'gpu'."
-    return 1
+if [ "$UNSET" == true ]; then
+    return
 fi
 
+# Set up appropriate environment
+MODE="cpu"
+if   [ "$CPU" == false ] && [ "$GPU" == true ]; then
+    MODE="gpu"
+    if ! hash nvidia-smi 2>/dev/null; then
+	echo "WARNING: Requesting GPUs on a node that doesn't have any. Exiting."
+	return 1
+    fi
+elif [ "$CPU" == "$GPU" ]; then
+    echo "Using CPU by default"
+fi
+module load anaconda cuda/8.0.61 root/6.06.02
+source activate adversarial-$MODE
+
 # Set up paths for interactive testing
-if [ ! -z "$TEST" ]; then
+if [ "$TEST" == true ]; then
+    echo "Setting I/O environment variables for interactive testing"
     export INPUTDIR="/exports/eddie/scratch/s1562020/adversarial/data/2017-08-25-ANN/"
     export OUTPUTDIR="/exports/eddie/scratch/s1562020/adversarial/output/test/"
 fi
 
-return 0
+return
