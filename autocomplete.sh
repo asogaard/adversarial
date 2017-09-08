15650514af260fcf74b008399db42175748a6425
Andreas Søgaard <andreas.sogaard@gmail.com>
2017-09-08 10:44:54 +0100

    Update autocomplete

--- autocomplete.sh
+++ autocomplete.sh
@@ -4,14 +4,14 @@ _submit()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="-h --help --gpu --train --tag --patch"
+    opts="-h --help --gpu --train --tensorflow --config --patch --tag --username"
 
     if [[ ${cur} == -* ]] ; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
         return 0
     fi
 }
-complete -F _submit ./submit.sh
+complete -F _submit -o default ./submit.sh
 
 _run() 
 {
@@ -26,4 +26,4 @@ _run()
         return 0
     fi
 }
-complete -F _run ./run.py
+complete -F _run -o default ./run.py
