99dbe9b7bed7ebd29a4557b4756d89439ea0a705
Andreas Søgaard <andreas.sogaard@gmail.com>
2017-09-14 07:43:29 +0100

    Implement gradient reversal layer for both backends

--- adversarial/layers.py
+++ adversarial/layers.py
@@ -5,120 +5,198 @@
 Adapted from: https://github.com/asogaard/AdversarialSubstructure/blob/master/layers.py
 """
 
+# Basic import(s)
+import logging as log
+
 # Numpy import(s)
 import numpy as np
+from scipy.stats import norm
 
 # Keras import(s)
-from keras import backend as K_
+from keras import backend as K
 from keras.engine.topology import Layer
 
 
 def gaussian (x, coeff, mean, width):
-    """ Utility function, wrapping the computation of a unit gaussian using Keras-backend methods. """ 
-    return coeff * K_.exp( - K_.square(x - mean) / 2. / K_.square(width)) / K_.sqrt( 2. * K_.square(width) * np.pi) 
+    """Compute a unit gaussian using Keras-backend methods.""" 
+    return coeff * K.exp( - K.square(x - mean) / 2. / K.square(width)) / K.sqrt( 2. * K.square(width) * np.pi) 
 
-class Posterior(Layer):
-    """ Custom layer, modelling the posterior probability distribution for the jet mass using a gaussian mixture model (GMM) """
 
-    def __init__(self, num_components, num_dimensions, **kwargs):
+def gaussian_integral_on_unit_interval (means, widths):
+    """Compute the integral of unit gaussians on the unit interval.
+
+    Args:
+        ...
+
+    Returns:
+        ...
+    """
+    return norm.cdf(1, loc=means, scale=widths) - norm.cdf(0, loc=means, scale=widths)
+
+
+class PosteriorLayer (Layer):
+    """Custom layer, modelling the posterior probability distribution for the jet mass using a gaussian mixture model (GMM)"""
+
+    def __init__ (self, gmm_components, gmm_dimensions, **kwargs):
         self.output_dim = 1 # Probability
-        self.num_components = num_components
-        self.num_dimensions = num_dimensions
-        super(Posterior, self).__init__(**kwargs)
+        self.gmm_components = gmm_components
+        self.gmm_dimensions = gmm_dimensions
+        super(PosteriorLayer, self).__init__(**kwargs)
 
-    def call(self, x, mask=None):
-        """ Main call-method of the layer. 
-        The GMM needs to be implemented (1) within this method and (2) using Keras backend functions in order for the error back-propagation to work properly 
+    def call (self, x, mask=None):
+        """Main call-method of the layer.
+
+        The GMM needs to be implemented (1) within this method and (2) using
+        Keras backend functions in order for the error back-propagation to work
+        properly.
         """
+
+        # Check(s)
+        #mask = (x < 0) | (x > 1)
+        #assert K.sum(mask) == 0, "Recieved input to PosteriorLayer outside of [0,1]: " + str(K.eval(x[mask]))
 
         # Unpack list of inputs
         coeffs = x[0]
-        means  = x[1:                            1 + 1 * self.num_dimensions]
-        widths = x[1 + 1 * self.num_dimensions : 1 + 2 * self.num_dimensions]
+        means  = x[1:                            1 + 1 * self.gmm_dimensions]
+        widths = x[1 + 1 * self.gmm_dimensions : 1 + 2 * self.gmm_dimensions]
         inputs = x[-1]
 
         # Compute the pdf from the GMM
         pdf = gaussian(inputs[:,0], coeffs[:,0], means[0][:,0], widths[0][:,0])
-        for d in range(1, self.num_dimensions):
+        for d in range(1, self.gmm_dimensions):
             pdf *= gaussian(inputs[:,d], 1, means[d][:,0], widths[d][:,0])
+            #pdf /= guassian_integral_on_unit_interval(means[d][:,0], widths[d][:,0])
             pass
-        for c in range(1, self.num_components):
+        for c in range(1, self.gmm_components):
             this_pdf = gaussian(inputs[:,0], coeffs[:,c], means[0][:,c], widths[0][:,c])
-            for d in range(1, self.num_dimensions):
+            for d in range(1, self.gmm_dimensions):
                 this_pdf *= gaussian(inputs[:,d], 1, means[d][:,c], widths[d][:,c])
+                #this_pdf /= guassian_integral_on_unit_interval(means[d][:,c], widths[d][:,c])
                 pass
             pdf += this_pdf
             pass
+
+        # @TODO:
+        # - Check that K.sum((x < 0) || (x > 1)) == 0
+        # - Normalise to unity on ([0,1])^gmm_dimensions
         
-        return K_.reshape(pdf, (K_.shape(pdf)[0], 1))
+        return K.reshape(pdf, (K.shape(pdf)[0], 1))
 
+    #def get_output_shape_for (self, input_shape):
+    #    return (input_shape[0], self.output_dim)
 
-    def get_output_shape_for(self, input_shape):
+    def compute_output_shape (self, input_shape):
         return (input_shape[0], self.output_dim)
 
     pass  
 
 
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-# From [https://github.com/fchollet/keras/issues/3119]
+# Implementing gradient reversal layer
 
-import theano
+if K.backend() == 'tensorflow':
+    # Tensorflow implementation based on
+    # [https://stackoverflow.com/questions/45099737/implement-theano-operation-in-tensorflow]
+    log.info("Implementing gradient reversal layer in TensorFlow")
+    
+    import tensorflow as tf
+    def ReverseGradient (hp_lambda):
+        """Function factory for gradient reversal."""
+        
+        def reverse_gradient_function (X, hp_lambda=hp_lambda):
+            """Flips the sign of the incoming gradient during training."""
+            try:
+                reverse_gradient_function.num_calls += 1
+            except AttributeError:
+                reverse_gradient_function.num_calls = 1
+                pass
+            
+            grad_name = "GradientReversal%d" % reverse_gradient_function.num_calls
+            
+            @tf.RegisterGradient(grad_name)
+            def _flip_gradients(op, grad):
+                return [tf.negative(grad) * hp_lambda]
+            
+            g = K.get_session().graph
+            with g.gradient_override_map({'Identity': grad_name}):
+                y = tf.identity(X)
+                pass
+            
+            return y
+        
+        return reverse_gradient_function
 
-class ReverseGradient(theano.Op):
-    """ theano operation to reverse the gradients
-    Introduced in http://arxiv.org/pdf/1409.7495.pdf
-    """
+else:
+    # Theano implementation based on
+    # [https://github.com/fchollet/keras/issues/3119#issuecomment-230289301]
+    log.info("Implementing gradient reversal layer in Theano")
+        
+    import theano
+    class ReverseGradient (theano.Op):
+        """Theano operation to reverse the gradients
+        Introduced in http://arxiv.org/pdf/1409.7495.pdf
+        """
+        
+        view_map = {0: [0]}
+        
+        __props__ = ('hp_lambda', )
+        
+        def __init__ (self, hp_lambda):
+            super(ReverseGradient, self).__init__()
+            self.hp_lambda = hp_lambda
+            
+        def make_node (self, x):
+            assert hasattr(self, '_props'), "Your version of theano is too old to support __props__."
+            x = theano.tensor.as_tensor_variable(x)
+            return theano.Apply(self, [x], [x.type()])
+        
+        def perform (self, node, inputs, output_storage):
+            xin, = inputs
+            xout, = output_storage
+            xout[0] = xin
+            
+        def grad (self, input, output_gradients):
+            return [-self.hp_lambda * output_gradients[0]]
+        
+        def infer_shape (self, node, i0_shapes):
+            return i0_shapes
+        
+        pass
+    
+    pass # end: tensorflow/theano
 
-    view_map = {0: [0]}
 
-    __props__ = ('hp_lambda', )
-
-    def __init__(self, hp_lambda):
-        super(ReverseGradient, self).__init__()
-        self.hp_lambda = hp_lambda
-
-    def make_node(self, x):
-        assert hasattr(self, '_props'), "Your version of theano is too old to support __props__."
-        x = theano.tensor.as_tensor_variable(x)
-        return theano.Apply(self, [x], [x.type()])
-
-    def perform(self, node, inputs, output_storage):
-        xin, = inputs
-        xout, = output_storage
-        xout[0] = xin
-
-    def grad(self, input, output_gradients):
-        return [-self.hp_lambda * output_gradients[0]]
-
-    def infer_shape(self, node, i0_shapes):
-        return i0_shapes
-
-    pass
-
-
-class GradientReversalLayer(Layer):
-    """ Reverse a gradient 
+# Layer implementation based on
+# [https://github.com/fchollet/keras/issues/3119#issuecomment-230289301]
+class GradientReversalLayer (Layer):
+    """Reverse a gradient 
     <feedforward> return input x
     <backward> return -lambda * delta
     """
-    def __init__(self, hp_lambda, **kwargs):
+    def __init__ (self, hp_lambda, **kwargs):
         super(GradientReversalLayer, self).__init__(**kwargs)
+        self.supports_masking = False
         self.hp_lambda = hp_lambda
         self.gr_op = ReverseGradient(self.hp_lambda)
-
-    def build(self, input_shape):
+        pass
+    
+    def build (self, input_shape):
         self.trainable_weights = []
-
-    def call(self, x, mask=None):
+        return
+    
+    def call (self, x, mask=None):
         return self.gr_op(x)
-
-    def get_output_shape_for(self, input_shape):
+    
+    #def get_output_shape_for (self, input_shape):
+    #    return input_shape
+    
+    def compute_output_shape (self, input_shape):
         return input_shape
-
-    def get_config(self):
+    
+    def get_config (self):
         config = {"name": self.__class__.__name__,
                   "lambda": self.hp_lambda}
         base_config = super(GradientReversalLayer, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-
+    
     pass
