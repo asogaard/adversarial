acbd91093a88656f07ad819a882e655b14c5bed8
Andreas Søgaard <andreas.sogaard@gmail.com>
2017-09-14 07:41:51 +0100

    Update autocomplete

--- scripts/autocomplete.sh
+++ scripts/autocomplete.sh
@@ -4,7 +4,7 @@ _submit()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="-h --help --gpu --train --tensorflow --folds --config --patch --tag --username"
+    opts="-h --help --gpu --train --tensorflow --devices --folds --config --patch --tag --username"
 
     if [[ ${cur} == -* ]] ; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
@@ -19,7 +19,7 @@ _run()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="-h --help -i --input -o --output -c --config -p --patch --threads --folds -v --verbose -g --gpu --tensorflow --train"
+    opts="-h --help -i --input -o --output -c --config -p --patch --devices --folds -v --verbose -g --gpu --tensorflow --train"
 
     if [[ ${cur} == -* ]] ; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
