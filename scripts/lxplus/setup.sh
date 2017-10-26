dc404cf6bd3a5db7445a136c46f5b715c5e9ec20
asogaard <andreas.sogaard@gmail.com>
2017-10-26 14:27:32 +0200

    Update setup scripts

---
+++ scripts/lxplus/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Initialise environment on lxplus
+
+echo "On lxplus"
+echo ">>$@<<"
+
+# Fix setup warning messages
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
+# Set up LCG 91 version of SWAN (latest stable)
+source /cvmfs/sft.cern.ch/lcg/views/LCG_91/x86_64-slc6-gcc62-opt/setup.sh
--- setup.sh
+++ setup.sh
@@ -3,69 +3,10 @@
 # necessary for running the adversarial 
 # neural network training an evaluation.
 
-# Extract flags
-ARGUMENTS=("$@")
-set -- # Unsetting positional arguments, to avoid error from "source deactivate"
-
-CPU=false
-GPU=false
-TEST=false
-UNSET=false
-for ARG in "${ARGUMENTS[@]}"; do
-    if   [ "$ARG" == "cpu" ];  then
-	CPU=true
-    elif [ "$ARG" == "gpu" ];  then
-	GPU=true
-    elif [ "$ARG" == "test" ]; then
-	TEST=true
-    elif [ "$ARG" == "unset" ]; then
-	UNSET=true
-    else
-	echo "Argument '$ARG' was not understood"
-    fi
-done
-
-# Deactivate any current environments
-# -- Check if command 'conda' exists
-# -- Check whether an environment is activated
-if hash conda 2>/dev/null && [ "$(conda info --envs | grep \* | grep -v root)" ]
-then
-    source deactivate
+if   [[ "$HOSTNAME" == *"lxplus"* ]]; then
+    source scripts/lxplus/setup.sh "$@"
+elif [[ "$HOSTNAME" == *"ed.ac.uk"* ]]; then
+    source scripts/eddie3/setup.sh "$@"
+else
+    echo "Host not recognised; unable to setup environment."
 fi
-
-# Unload any existing modules on Eddie
-module unload $(module list 2>&1 | tail -n +2 | sed 's/ *[0-9]*) \([a-z]*\)\/[0-9._a-z]*/\1 /g' | sed 's/sge *//g')
-
-if [ "$UNSET" == true ]; then
-    return
-fi
-
-# Set up appropriate environment
-MODE="cpu"
-if   [ "$CPU" == false ] && [ "$GPU" == true ]; then
-    MODE="gpu"
-    if ! hash nvidia-smi 2>/dev/null; then
-	echo "WARNING: Requesting GPUs on a node that doesn't have any. Exiting."
-	return 1
-    fi
-elif [ "$CPU" == "$GPU" ]; then
-    echo "Using CPU by default"
-fi
-module load anaconda cuda/8.0.61 root/6.06.02
-source activate adversarial-$MODE
-
-# Set up paths for interactive testing
-if [ "$TEST" == true ]; then
-    echo "Setting I/O environment variables for interactive testing"
-    #export
-    #INPUTDIR="/exports/eddie/scratch/s1562020/adversarial/data/2017-08-25-ANN/"
-    #export
-    #INPUTDIR="/exports/eddie/scratch/s1562020/adversarial/data/2017-09-08-ANN/"
-    export INPUTDIR="/exports/eddie/scratch/s1562020/adversarial/data/prepared/"
-    export OUTPUTDIR="/exports/eddie/scratch/s1562020/adversarial/output/test/"
-fi
-
-# Enable auto-complete for command-line arguments
-source scripts/autocomplete.sh
-
-return
