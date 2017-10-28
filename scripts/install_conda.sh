02cd5bd7f9f63105c863db086487508d5e6c8f30
asogaard <andreas.sogaard@gmail.com>
2017-10-28 12:49:10 +0200

    Add conda installation script

---
+++ scripts/install_conda.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+# Follow the screen prompts
+# ...
+rm Miniconda3-latest-Linux-x86_64.sh
