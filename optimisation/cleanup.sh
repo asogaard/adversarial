b9057157ccb1d3345fa353e4ca5e60f1d9d939d0
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-01-12 09:39:25 +0000

    Make cross-platform compatible; add compatibility flag to xargs

--- optimisation/cleanup.sh
+++ optimisation/cleanup.sh
@@ -9,8 +9,15 @@ if [ -d "$dir" ]; then
 	rm -f $dir/experiments/*/*.pyc
 	rm -f $dir/log.txt*
 	rm -rf $dir/db/*
-  find $dir -name *~ | xargs rm
-	ps -u asogaard | grep mongod | grep -v grep | sed 's/^ *//g' | cut -d" " -f2 | xargs kill
+	find $dir -name *~ | xargs -r rm
+	
+	# Get index if `pid` field in `ps -u ...`
+	pid_field=1
+	if [ "$(uname)" == *"Darwin"* ]; then
+	    pid_field=2
+	fi
+	
+	ps -u `whoami` | grep mongod | grep -v grep | sed 's/^ *//g' | cut -d" " -f$pid_field | xargs -r kill
     else
 	echo "Directory $dir/ exists, but has no subdirectory $dir/experiment/."
     fi
