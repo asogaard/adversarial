0e5c013f8fd70a379ca2ab264803165a3903668f
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-01-17 10:54:01 +0000

    Update start_mongo script

--- optimisation/scripts/start_mongo.sh
+++ optimisation/scripts/start_mongo.sh
@@ -4,53 +4,47 @@
 # Import utility methods
 source scripts/utils.sh
 
-# Base directory
+# Define variable(s)
 dir="optimisation"
 
-if [ -d "$dir" ]; then
-
-    # Check whether a MongoDB server is already running
-    if [[ ! -z "$(ps -u `whoami` | grep mongod | grep -v grep)" ]]; then
-        warning "A MongoDB server is already running. To restart, please run stop.sh first."
-        return 1
-    fi
-
-    # Make sure that directories exist
-    logpath="logs/"
-    dbpath="$dir/db"
-
-    mkdir -p $logpath
-    mkdir -p $dbpath
-
-    # Start server
-    mongod_arguments="--fork --logpath $logpath/mongo.log --dbpath $dbpath/"
-    mongod $mongod_arguments
-
-    # Check return code
-    ret="$?"
-    if (( "$ret" )); then
-        warning "Recieved return code $ret."
-
-        if (( "$ret" == 100 )); then
-            rm -f $dir/db/mongod.lock
-
-            # Try to start server again
-            print "Try to start server again."
-            return 1
-        fi
-
-    fi
-
-    # Perform repair
-    #source $dir/scripts/repair.sh
-
-    # Perform cleanup
-    source $dir/scripts/cleanup.sh
-
-    print "Remember to run spearmint.main yourself!"
-    return 0
-
-else
+# Check(s)
+if [ ! -d "$dir" ]; then
     warning "Directory $dir/ doesn't exist."
     return 1
 fi
+
+if is_running mongod; then
+    warning "A MongoDB server is already running. To restart, please run stop.sh first."
+    return 1
+fi
+
+# Make sure that directories exist
+logpath="logs/"
+dbpath="$dir/db"
+
+mkdir -p $logpath
+mkdir -p $dbpath
+
+# Start server
+mongod_arguments="--fork --logpath $logpath/mongo.log --dbpath $dbpath/"
+mongod $mongod_arguments
+
+# Check return code
+ret="$?"
+if (( "$ret" )); then
+    warning "Recieved return code $ret."
+
+    if (( "$ret" == 100 )); then
+        rm -f $dir/db/mongod.lock
+
+        # Try to start server again
+        print "Try to start server again."
+        return 1
+    fi
+
+fi
+
+# Perform cleanup
+source $dir/scripts/cleanup.sh
+
+print "Remember to run spearmint.main yourself!"
