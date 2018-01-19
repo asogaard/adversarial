6608f6b7a85c2e8feb59435636c68717d34a59ad
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-01-19 16:49:24 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = "adversarial",
+    version = "0.1",
+    author = "Andreas Sogaard",
+    author_email = "andreas.sogaard@ed.ac.uk",
+    description = ("Tools for training and evaluating de-correlated jet taggers"),
+    keywords = "Adversarial neural networks, jet substructure",
+    url="https://github.com/asogaard/adversarial",
+    packages=['adversarial',
+              'optimisation',
+              'run',
+              'run.adversarial',
+              'run.uboost',
+              ],
+    )
