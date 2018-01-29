43c196d12ef091e5c4ea3f287ab0899b91663fdb
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-01-29 13:09:02 +0000

    Refactor kNN code

---
+++ run/knn/common.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Common methods for training and testing fixed-efficiency kNN regressor."""
+
+# Basic import(s)
+import itertools
+
+# Scientific import(s)
+import ROOT
+import numpy as np
+import pandas as pd
+
+# Project import(s)
+from adversarial.utils import wpercentile
+from adversarial.profile import profile
+
+
+# Common definitions
+VAR  = 'D2'   # Substructure variable to decorrelate
+EFF  = 20     # Fixed backround efficiency at which to perform decorrelation
+VARX = 'rho'  # X-axis variable from which to decorrelate
+VARY = 'pt'   # Y-axis variable from which to decorrelate
+AXIS = {      # Dict holding (num_bins, axis_min, axis_max) for axis variables
+    'rho': (20, -5.5, -2.0),
+    'pt':  (20, 200., 2000.),
+}
+
+
+@profile
+def add_variables (data):
+    """Add necessary variable(s).
+    Modify data container in-place."""
+
+    # rho
+    data['rho'] = pd.Series(np.log(np.square(data['m']) / np.square(data['pt'])), index=data.index)
+    return
+
+
+@profile
+def fill_profile (data):
+    """Fill ROOT.TH2F with the measured, weighted values of the `EFF`-percentile
+    of the background `VAR`. """
+
+    # Define arrays
+    shape = (AXIS[VARX][0], AXIS[VARY][0])
+    binsx = np.linspace(AXIS[VARX][1], AXIS[VARX][2], AXIS[VARX][0] + 1, endpoint=True)
+    binsy = np.linspace(AXIS[VARY][1], AXIS[VARY][2], AXIS[VARY][0] + 1, endpoint=True)
+    x = np.zeros(shape)
+    y = np.zeros_like(x)
+    z = np.zeros_like(x)
+
+    # Create `profile` histogram
+    profile = ROOT.TH2F('profile', "", len(binsx) - 1, binsx.flatten('C'), len(binsy) - 1, binsy.flatten('C'))
+
+    # Fill profile
+    for i,j in itertools.product(*map(range, shape)):
+
+        # Bin edges in x and y
+        xmin, xmax = binsx[i:i+2]
+        ymin, ymax = binsy[j:j+2]
+
+        # Masks
+        mskx = (data[VARX] > xmin) & (data[VARX] <= xmax)
+        msky = (data[VARY] > ymin) & (data[VARY] <= ymax)
+        msk  = mskx & msky
+
+        # Percentile
+        perc = np.nan
+        if np.sum(msk) > 20:  # Ensure sufficient statistics for meaningful percentile
+            perc = wpercentile(data=   data.loc[msk, VAR]     .as_matrix().astype(np.float).flatten(), percents=EFF,
+                               weights=data.loc[msk, 'weight'].as_matrix().astype(np.float).flatten())
+            pass
+        x[i,j] = (xmin + xmax) * 0.5
+        y[i,j] = (ymin + ymax) * 0.5
+        z[i,j] = perc
+
+        # Set non-zero bin content
+        if perc == perc:
+            profile.SetBinContent(i + 1, j + 1, perc)
+            pass
+        pass
+
+    # Normalise arrays
+    x -= AXIS[VARX][1]
+    x /= AXIS[VARX][2] - AXIS[VARX][1]
+    y -= AXIS[VARY][1]
+    y /= AXIS[VARY][2] - AXIS[VARY][1]
+
+    return profile, (x,y,z)
---
+++ run/knn/test.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Script for testing fixed-efficiency kNN regressor."""
+
+# Basic import(s)
+import gzip
+import pickle
+
+# Scientific import(s)
+import ROOT
+import numpy as np
+
+# Project import(s)
+from adversarial.utils import latex
+from adversarial.profile import profile, Profile
+from adversarial.new_utils import parse_args, initialise, load_data
+from adversarial.constants import *
+
+# Local import(s)
+from run.knn.common import *
+
+# Custom import(s)
+import rootplotting as rp
+
+
+# Main function definition
+@profile
+def main (args):
+
+    # Initialising
+    # --------------------------------------------------------------------------
+    args, cfg = initialise(args)
+
+
+    # Loading data
+    # --------------------------------------------------------------------------
+    data, _, _ = load_data(args.input + 'data.h5')
+    data = data[(data['train'] == 0) & (data['signal'] == 0)]
+
+
+    # Common definitions
+    # --------------------------------------------------------------------------
+    bounds = [
+        ROOT.TF1('bounds_0', "TMath::Sqrt( TMath::Power( 40, 2) * TMath::Exp(-x) )", AXIS[VARX][1], AXIS[VARX][2]),
+        ROOT.TF1('bounds_1', "TMath::Sqrt( TMath::Power(300, 2) * TMath::Exp(-x) )", AXIS[VARX][1], AXIS[VARX][2])
+        ]
+    ROOT.gStyle.SetPalette(53)
+    nb_contour = 13 * 16
+
+    for bound in bounds:
+        bound.SetLineColor(ROOT.kGray + 2)
+        bound.SetLineWidth(1)
+        pass
+
+
+    # Adding variable(s)
+    # --------------------------------------------------------------------------
+    add_variables(data)
+
+
+    # Filling profile
+    # --------------------------------------------------------------------------
+    profile_meas, _ = fill_profile(data)
+
+
+    # Plotting profile
+    # --------------------------------------------------------------------------
+    with Profile("Plotting profile"):
+
+        # rootplotting
+        c = rp.canvas(batch=True)
+        pad = c.pads()[0]._bare()
+        pad.cd()
+        pad.SetRightMargin(0.20)
+        pad.SetLeftMargin(0.15)
+        pad.SetTopMargin(0.10)
+
+        # Styling
+        profile_meas.GetXaxis().SetTitle("Large-#it{R} jet " + latex(VARX, ROOT=True) + " = log(m^{2}/p_{T}^{2})")
+        profile_meas.GetYaxis().SetTitle("Large-#it{R} jet " + latex(VARY, ROOT=True) + " [GeV]")
+        profile_meas.GetZaxis().SetTitle("Measured, weighted {}-percentile of {}".format(EFF, latex(VAR, ROOT=True)))
+        profile_meas.GetYaxis().SetTitleOffset(1.6)
+        profile_meas.GetZaxis().SetTitleOffset(1.4)
+        profile_meas.GetZaxis().SetRangeUser(0.7, 2.0)
+        profile_meas.SetContour(nb_contour)
+
+        # Draw
+        profile_meas.Draw('COLZ')
+        bounds[0].DrawCopy("SAME")
+        bounds[1].DrawCopy("SAME")
+        c.latex("m > 40 GeV",  -4.5, bounds[0].Eval(-4.5) + 30, align=21, angle=-20, textsize=13, textcolor=ROOT.kGray + 2)
+        c.latex("m < 300 GeV", -3.2, bounds[1].Eval(-3.2) - 30, align=23, angle=-53, textsize=13, textcolor=ROOT.kGray + 2)
+
+        # Decorations
+        c.text(qualifier=QUALIFIER,
+               ymax=0.92,
+               xmin=0.15)
+        c.text(["#sqrt{s} = 13 TeV,  QCD jets",
+                "Training dataset",
+                "Baseline selection",
+                ],
+            ATLAS=False,
+            ymax=0.83,
+            xmin=0.18,
+            textcolor=ROOT.kWhite)
+        c.save('figures/knn_profile.pdf')
+        pass
+
+
+    # Loading KNN classifier
+    # --------------------------------------------------------------------------
+    with Profile("Loading KNN classifier"):
+        with gzip.open('models/knn/knn_{:s}_{:.0f}.pkl.gz'.format(VAR, EFF), 'r') as f:
+            knn = pickle.load(f)
+            pass
+        pass
+
+
+    # Plotting fit
+    # --------------------------------------------------------------------------
+    with Profile("Plotting fit"):
+        rebin = 10
+        binsx_fine = np.interp(np.linspace(0, AXIS[VARX][0], AXIS[VARX][0] * rebin + 1, endpoint=True), range(AXIS[VARX][0] + 1), np.linspace(AXIS[VARX][1], AXIS[VARX][2], AXIS[VARX][0] + 1, endpoint=True))
+        binsy_fine = np.interp(np.linspace(0, AXIS[VARY][1], AXIS[VARY][1] * rebin + 1, endpoint=True), range(AXIS[VARY][0] + 1), np.linspace(AXIS[VARY][1], AXIS[VARY][2], AXIS[VARY][0] + 1, endpoint=True))
+
+        gx, gy = np.meshgrid(binsx_fine, binsy_fine)
+        gx -= AXIS[VARX][1]
+        gx /= AXIS[VARX][2] - AXIS[VARX][1]
+        gy -= AXIS[VARY][1]
+        gy /= AXIS[VARY][2] - AXIS[VARY][1]
+        X_fine = np.vstack((gx.flatten(), gy.flatten())).T
+        fit = knn.predict(X_fine).reshape(gx.shape).T
+
+        # Fill ROOT "profile"
+        profile_fit = ROOT.TH2F('profile_fit', "", len(binsx_fine) - 1, binsx_fine.flatten('C'), len(binsy_fine) - 1, binsy_fine.flatten('C'))
+        for i,j in itertools.product(*map(range, map(len,[binsx_fine, binsy_fine]))):
+            x,y = binsx_fine[i], binsy_fine[j]
+            x -= AXIS[VARX][1]
+            x /= AXIS[VARX][2] - AXIS[VARX][1]
+            y -= AXIS[VARY][1]
+            y /= AXIS[VARY][2] - AXIS[VARY][1]
+            pred = knn.predict([[x,y]])
+            profile_fit.SetBinContent(i + 1, j + 1, pred)
+            pass
+
+        # rootplotting
+        c = rp.canvas(batch=True)
+        pad = c.pads()[0]._bare()
+        pad.cd()
+        pad.SetRightMargin(0.20)
+        pad.SetLeftMargin(0.15)
+        pad.SetTopMargin(0.10)
+
+        # Styling
+        profile_fit.GetXaxis().SetTitle("Large-#it{R} jet " + latex(VARX, ROOT=True) + " = log(m^{2}/p_{T}^{2})")  # @TODO: Improve...
+        profile_fit.GetYaxis().SetTitle("Large-#it{R} jet " + latex(VARY, ROOT=True) + " [GeV]")
+        profile_fit.GetZaxis().SetTitle("kNN-fitted, weighted {}-percentile of {}".format(EFF, latex(VAR, ROOT=True)))
+        profile_fit.GetYaxis().SetTitleOffset(1.6)
+        profile_fit.GetZaxis().SetTitleOffset(1.4)
+        profile_fit.GetZaxis().SetRangeUser(0.7, 2.0)
+        profile_fit.SetContour(nb_contour)
+
+        # Draw
+        profile_fit.Draw('COLZ')
+        bounds[0].DrawCopy("SAME")
+        bounds[1].DrawCopy("SAME")
+        c.latex("m > 40 GeV",  -4.5, bounds[0].Eval(-4.5) + 30, align=21, angle=-20, textsize=13, textcolor=ROOT.kGray + 2)
+        c.latex("m < 300 GeV", -3.2, bounds[1].Eval(-3.2) - 30, align=23, angle=-53, textsize=13, textcolor=ROOT.kGray + 2)
+
+        # Decorations
+        c.text(qualifier=QUALIFIER,
+               ymax=0.92,
+               xmin=0.15)
+        c.text(["#sqrt{s} = 13 TeV,  QCD jets",
+                "Training dataset",
+                "Baseline selection",
+                ],
+            ATLAS=False,
+            ymax=0.83,
+            xmin=0.18,
+            textcolor=ROOT.kWhite)
+        c.save('figures/knn_fit.pdf')
+        pass
+
+    return 0
+
+
+# Main function call
+if __name__ == '__main__':
+
+    # Parse command-line arguments
+    args = parse_args()
+
+    # Call main function
+    main(args)
+    pass
