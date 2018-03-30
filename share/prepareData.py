c0dcd3d4e5d631cd9ee18b62de3daeace056b773
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-03-30 09:59:03 +0100

    Update data preparation script

--- share/prepareData.py
+++ share/prepareData.py
@@ -31,6 +31,97 @@ parser.add_argument('-o', '--output', dest='output', action='store', type=str,
                     default='/exports/eddie/scratch/s1562020/adversarial/data/prepared/',
                     help='Output directory, to which to write output files.')
 
+# Global definition(s)
+SELECTION = {
+    'common': ("(fjet_truthJet_pt >   200E+03 && "
+               " fjet_truthJet_pt < 2000E+03 && "
+               " fjet_truthJet_eta > -2.0 && "
+               " fjet_truthJet_eta <  2.0 && "
+               " fjet_m >  50E+03 && "
+               " fjet_m < 300E+03 &&"
+               " fjet_numConstituents > 2)"),
+
+    'sig': ("(W && "
+            " fjet_dRmatched_WZChild1_dR < 0.75 && "
+            " fjet_dRmatched_WZChild2_dR < 0.75 && "
+            " fjet_truth_dRmatched_particle_dR < 0.75)"),
+
+    'bkg': ("(!W &&
+            " !top)")
+    }
+
+# -- Add `common` selection to all other keys
+common_selection = SELECTION.pop('common')
+for key in SELECTION.keys():
+    SELECTION[key] += " && " + common_selection
+    pass
+
+# -- 'Wtop' indicates use for W-tagging DNN
+BRANCHES = [
+    # Kinematics
+    'fjet_CaloTACombinedMassUncorrelated', # Combined jet mass | Wtop
+    'fjet_JetpTCorrByCombinedMass',        # Corrected jet pT  | Wtop
+    'fjet_phi', # Kept for control
+    'fjet_eta', # Kept for control
+
+    # Event-level info
+    'EventInfo_NPV',   # Kept for control
+    'fjet_nthLeading', # @TODO: Check. Only use == 1?
+
+    # Substructure variables.
+    'fjet_Tau1_wta',
+    'fjet_Tau2_wta',
+    'fjet_Tau3_wta',
+    'fjet_Tau21_wta', # Wtop
+    'fjet_Tau32_wta',
+
+    'fjet_ECF1',
+    'fjet_ECF2',
+    'fjet_ECF3',
+    'fjet_e2',
+    'fjet_e3',
+    'fjet_C2', # Wtop
+    'fjet_D2', # Wtop
+
+    'fjet_Angularity',   # Wtop
+    'fjet_Aplanarity',   # Wtop
+    'fjet_Dip12',
+    'fjet_FoxWolfram20', # Wtop
+    'fjet_KtDR',         # Wtop
+    'fjet_PlanarFlow',   # Wtop
+    'fjet_Sphericity',
+    'fjet_Split12',      # Wtop
+    'fjet_Split23',
+    'fjet_ThrustMaj',
+    'fjet_ThrustMin',
+    'fjet_ZCut12',       # Wtop
+    'fjet_Qw',
+    'fjet_Mu12',
+
+    'fjet_SDt_Dcut1',
+    'fjet_SDt_Dcut2',
+
+    # Weights
+    'fjet_testing_weight_pt',
+    'fjet_training_weight_pt_W',
+
+    # Signal flag
+    'W',
+    ]
+
+
+def rename (name):
+    """Rename ntuple branches to for consistency with naming in project code"""
+    if name == 'W':
+        return 'signal'
+    name = name.replace('fjet_', '')
+    name = name.replace('_wta', '')
+    name = name.replace('CaloTACombinedMassUncorrelated', 'm')
+    name = name.replace('JetpTCorrByCombinedMass', 'pt')
+    name = name.replace('training_weight_pt_W', 'weight_train')
+    name = name.replace('testing_weight_pt', 'weight_test')
+    return name
+
 
 # Main function definition
 @profile
@@ -59,101 +150,14 @@ def main ():
 
         paths = [args.input + f for f in files]
 
-        # @TODO: Add |fjet_truthJet_eta| < 2.0?
-        selection = {
-            'sig': ("(W && "
-                    "fjet_truthJet_pt > 200E+03 && "
-                    "fjet_truthJet_pt < 2000E+03 && "
-                    "fjet_m > 40E+03 && "
-                    "fjet_numConstituents > 2 && "
-                    "fjet_dRmatched_WZChild1_dR < 0.75 && "
-                    "fjet_dRmatched_WZChild2_dR < 0.75 && "
-                    "fjet_truth_dRmatched_particle_dR < 0.75)"),
-
-            'bkg': ("(!W && !top && "
-                    "fjet_truthJet_pt > 200E+03 && "
-                    "fjet_truthJet_pt < 2000E+03 && "
-                    "fjet_m > 40E+03 && "
-                    "fjet_numConstituents > 2)")
-            }
-
-        # 'Wtop' indicates use for W-tagging DNN
-        branches = [
-            # Kinematics
-            'fjet_CaloTACombinedMassUncorrelated', # Combined jet mass | Wtop
-            'fjet_JetpTCorrByCombinedMass',        # Corrected jet pT  | Wtop
-            'fjet_phi', # Kept for control
-            'fjet_eta', # Kept for control
-
-            # Event-level info
-            'EventInfo_NPV',   # Kept for control
-            'fjet_nthLeading', # @TODO: Check. Only use == 1?
-
-            # Substructure variables.
-            'fjet_Tau1_wta',
-            'fjet_Tau2_wta',
-            'fjet_Tau3_wta',
-            'fjet_Tau21_wta', # Wtop
-            'fjet_Tau32_wta',
-
-            'fjet_ECF1',
-            'fjet_ECF2',
-            'fjet_ECF3',
-            'fjet_e2',
-            'fjet_e3',
-            'fjet_C2', # Wtop
-            'fjet_D2', # Wtop
-
-            'fjet_Angularity',   # Wtop
-            'fjet_Aplanarity',   # Wtop
-            'fjet_Dip12',
-            'fjet_FoxWolfram20', # Wtop
-            'fjet_KtDR',         # Wtop
-            'fjet_PlanarFlow',   # Wtop
-            'fjet_Sphericity',
-            'fjet_Split12',      # Wtop
-            'fjet_Split23',
-            'fjet_ThrustMaj',
-            'fjet_ThrustMin',
-            'fjet_ZCut12',       # Wtop
-            'fjet_Qw',
-            'fjet_Mu12',
-
-            'fjet_SDt_Dcut1',
-            'fjet_SDt_Dcut2',
-
-            # Weights
-            'fjet_testing_weight_pt',
-            'fjet_training_weight_pt_W',
-
-            # Signal flag
-            'W',
-            ]
-
-        def rename (name):
-            """Rename ntuple branches to for consistency with naming in project code"""
-            if name == 'W':
-                return 'signal'
-            name = name.replace('fjet_', '')
-            name = name.replace('_wta', '')
-            name = name.replace('CaloTACombinedMassUncorrelated', 'm')
-            name = name.replace('JetpTCorrByCombinedMass', 'pt')
-            name = name.replace('training_weight_pt_W', 'weight_flat')
-            name = name.replace('testing_weight_pt', 'weight')
-            return name
-
         data = None
-        for key in selection.keys():
+        for key in SELECTION.keys():
             for path in paths:
                 f = ROOT.TFile(path, 'r')
                 t = f.Get('FlatSubstructureJetTree')
-                a = root_numpy.tree2array(t, branches=branches, selection=selection[key])
+                a = root_numpy.tree2array(t, branches=BRANCHES, selection=SELECTION[key])
                 a.dtype.names = map(rename, a.dtype.names)
-                if data is None:
-                    data = a
-                else:
-                    data = np.concatenate((data, a))
-                    pass
+                data = a if (data is None) else np.concatenate((data, a))
                 pass
             pass
 
@@ -170,16 +174,13 @@ def main ():
     # --------------------------------------------------------------------------
     with Profile("writing"):
 
-        # Make sure output directory exists
-        mkdir(args.output)
-
         # Save as HDF5
+        mkdir(args.output)
         with h5py.File(args.output + 'data.h5', 'w') as hf:
             hf.create_dataset('dataset',  data=data)
             pass
 
         pass
-
 
     # Testing reading in HDF5 file(s)
     # --------------------------------------------------------------------------
