399675a440d7062fe4dc72081ac1f0b4cf612c3c
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-04-11 13:10:22 +0100

    Update NN classifier optimisation setup

--- optimisation/experiments/classifier/optimise.py
+++ optimisation/experiments/classifier/optimise.py
@@ -5,6 +5,7 @@ import json
 
 # Project import(s)
 import adversarial
+from adversarial.utils import mkdir, parse_args
 from run.adversarial import train
 
 # Global variables
@@ -12,26 +13,31 @@ PROJECTDIR='/'.join(os.path.realpath(adversarial.__file__).split('/')[:-2] + [''
 
 
 class cd:
-    """Context manager for changing the current working directory.
-    From: [https://stackoverflow.com/a/13197763]"""
-
+    """
+    Context manager for changing the current working directory.
+    From: [https://stackoverflow.com/a/13197763]
+    """
+    
     def __init__(self, newPath):
         self.newPath = os.path.expanduser(newPath)
         return
-
+    
     def __enter__(self):
         self.savedPath = os.getcwd()
+        print "cd: Going to {}".format(self.newPath)
         os.chdir(self.newPath)
         return
-
+    
     def __exit__(self, etype, value, traceback):
+        print "cd: Going back to {}".format(self.savedPath)
         os.chdir(self.savedPath)
         return
     pass
 
 
 def create_patch (params, path):
-    """Create temporary patch file to use in optimisation.
+    """
+    Create temporary patch file to use in optimisation.
 
     The method traverses the entries in dict `params`, which have a hierarchial
     structure
@@ -40,9 +46,9 @@ def create_patch (params, path):
     The patch is saved as JSON file in `path` with structure
         # JSON file `path`
         ...
-        "first-level" : {
-            "second-level" : {
-                "third-level" : value,
+        'first-level' : {
+            'second-level' : {
+                'third-level' : value,
                 ...
             }
             ...
@@ -53,7 +59,7 @@ def create_patch (params, path):
     # Initialise output dictionary
     result = dict()
 
-    # Loop parameters, estable dictionary structure
+    # Loop parameters, establish dictionary structure
     for param in params:
         print "param: {}".format(param)
         entry = result
@@ -78,16 +84,8 @@ def create_patch (params, path):
     print "- " * 40
 
     # -- Make sure target directory exists
-    if '/' in path:
-        directory = '/'.join(path.split('/')[:-1])
-        if not os.path.exists(directory):
-            try:
-                os.makedirs(directory)
-            except OSError:
-                # "File exists" error -- for some reason.
-                pass
-            pass
-        pass
+    directory = '/'.join(path.split('/')[:-1])
+    mkdir(directory)
 
     # -- Dump JSON to file
     with open(path, 'w') as patchfile:
@@ -111,12 +109,21 @@ def main(job_id, params):
 
     # Set arguments
     # @TODO: Dynamically decide `--gpu`, `--devices N`?
-    args = train.parse_args(['--optimise-classifier', '--patch', patch, '--jobname', 'classifier-' + jobname ,'--tensorflow', '--gpu', '--devices', '6', '--folds', '5'])
-    print args
+    args = parse_args(['--optimise-classifier',
+                       '--patch',   patch,
+                       '--jobname', 'classifier-' + jobname,
+                       '--gpu',
+                       '--devices', '3',
+                       '--folds',   '3',
+                       '--tensorboard'],
+                      adversarial=True)
 
-    # Call main script
-    result = train.main(args)
+    # Call main script (in the correct directory)
+    with cd(PROJECTDIR):
+        result = train.main(args)
+        pass
 
     # Ensure correct type, otherwise Spearmint does not accept value
     result = float(result)
+    
     return result
