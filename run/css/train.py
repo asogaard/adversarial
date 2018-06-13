5ac625d30cb2f33d82cb0f1fc5503610252111af
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-06-13 10:49:46 +0100

    Remove rebinning for KDE; add sanity check plots

--- run/css/train.py
+++ run/css/train.py
@@ -7,11 +7,16 @@
 import gzip
 import pickle
 from array import array
+import itertools
 
 # Scientific import(s)
 import ROOT
+import root_numpy
 import numpy as np
 from scipy.special import gamma
+
+ROOT.gROOT.SetBatch(True)
+ROOT.gStyle.SetOptStat(0)
 
 # Project import(s)
 from adversarial.utils import parse_args, initialise, load_data, mkdir, latex, wpercentile
@@ -25,7 +30,6 @@ import rootplotting as rp
 # Local import(s)
 from .common import *
 
-REBIN = 5
 
 # Main function definition
 @profile
@@ -38,7 +42,6 @@ def main (args):
     data, features, _ = load_data(args.input + 'data.h5', background=True, train=True)
 
     # Fill substructure profile
-    D2BINS = np.linspace(0., 5., 501, endpoint=True)
     perform_optimisation("D2", D2BINS, data)
     return
 
@@ -50,12 +53,12 @@ def perform_optimisation (var, bins, data):
     """
 
     # Fill 2D substructure profile
-    msk_train = data['train'] == 1
     profile2d = fill_2d_profile(data, var, bins, "m", MASS_BINS)
 
     # Get 1D profile for lowest mass bin
     profile0 = profile2d.ProjectionY("%s_lowMass"%profile2d.GetName(), 1, 1)
     profile0 = kde(profile0)
+    normalise(profile0, density=True)
 
     # Perform the optimisation
     bestShapeVal = 0
@@ -66,11 +69,10 @@ def perform_optimisation (var, bins, data):
 
         # Each mass bin needs to be optimized over omega
         for mass in range(len(MASS_BINS)-1):
-            print "Mass bin: ", mass
+            print "   Mass bin: ", mass
 
             # Get 1D profile for current mass bin
             profile = profile2d.ProjectionY("%s_bin_%i"%(profile2d.GetName(),mass),mass+1, mass+1)
-            profile = kde(profile)
 
             # Fit current profile to low-mass profile
             chi2, bestOmega, _, _ = fit(profile, shapeVal, profile0, "%.2f"%mass)
@@ -80,6 +82,7 @@ def perform_optimisation (var, bins, data):
             pass
 
         # Update chi2 for current `shapeVal`
+        print "-- sumChi2: {} (cp. {})".format(sumChi2, bestSumChi2)
         if sumChi2 < bestSumChi2:
             bestSumChi2  = sumChi2
             bestShapeVal = shapeVal
@@ -92,18 +95,75 @@ def perform_optimisation (var, bins, data):
         # Ensure model directory exists
         mkdir('models/css/')
 
-        # Write out the optifmal configuration for each mass bin
+        # Get the optimal, measured `omega`s for each mass-bin
         bestOmegas = list()
         for mass in range(len(MASS_BINS)-1):
             profile = profile2d.ProjectionY("%s_bin_%i_final"%(profile2d.GetName(),mass),mass+1, mass+1)
-            profile = kde(profile)
             sumChi2, bestOmega, profile_css, profile0rebin = fit(profile, bestShapeVal, profile0, "%.2f"%mass)
+
+            # Test-plot distributions used for fitting!
+            # -- Canvas
+            c = rp.canvas(batch=True)
+
+            # -- Plot
+            profile = kde(profile)
+            normalise(profile, density=True)
+
+            lowmassbin = "#it{{m}} #in  [{:.1f}, {:.1f}] GeV".format(MASS_BINS[0],    MASS_BINS[1])     .replace('.0', '')
+            massbin    = "#it{{m}} #in  [{:.1f}, {:.1f}] GeV".format(MASS_BINS[mass], MASS_BINS[mass+1]).replace('.0', '')
+            c.hist(profile0rebin, label=latex(var, ROOT=True) + ",    {}".format(lowmassbin),
+                   linecolor=rp.colours[1], fillcolor=rp.colours[1], alpha=0.5, option='HISTL', legend_option='FL')
+            c.hist(profile,       label=latex(var, ROOT=True) + ",    {}".format(massbin),
+                   linecolor=rp.colours[4], linestyle=2, option='HISTL')
+            c.hist(profile_css,   label=latex(var + 'CSS', ROOT=True) + ", {}".format(massbin),
+                   linecolor=rp.colours[3], option='HISTL')
+
+            # -- Decorations
+            c.xlabel(latex(var, ROOT=True) + ", " + latex(var + 'CSS', ROOT=True))
+            c.ylabel("Number of jets p.d.f.")
+            c.legend(xmin=0.45, ymax=0.76, width=0.25)
+            c.text(["#sqrt{s} = 13 TeV,  Multijets",
+                    "KDE smoothed"], qualifier=QUALIFIER)
+            c.pad()._xaxis().SetTitleOffset(1.3)
+            c.pad()._yaxis().SetNdivisions(105)
+            c.pad()._primitives[-1].Draw('SAME AXIS')
+            c.padding(0.50)
+
+
+            # -- Save
+            c.save('figures/css/css_test_{}_mass{}.pdf'.format(var, mass))
+
+            # Store best-fit omega in array
             print mass, bestOmega
             bestOmegas.append(bestOmega)
-            F,Ginv = get_css_fns(bestShapeVal, bestOmega, profile, "")
+            pass
 
+        # Fit best omega vs. mass
+        x = MASS_BINS[:-1] + 0.5 * np.diff(MASS_BINS)
+        y = np.array(bestOmegas)
 
-            # Save plot of histograms used for the training, for cross-checks
+        h = ROOT.TH1F('hfit', "", len(MASS_BINS) - 1, MASS_BINS)
+        root_numpy.array2hist(y, h)
+        for ibin in range(1, len(x) + 1):
+            h.SetBinError(ibin, 0.02)  # Just some value to ensure equal errors on all points
+            pass
+
+        m0 = 0.5 * (MASS_BINS[0] + MASS_BINS[1])
+        f = ROOT.TF1("fit","[0] * (1./{m0}  - 1./x) + [1] * TMath::Log(x/{m0})".format(m0=m0), m0, 300);
+        f.SetLineColor(rp.colours[4])
+        f.SetLineStyle(2)
+        h.Fit(f)
+
+        # Write out the optimal configuration for each mass bin
+        for mass in range(len(MASS_BINS)-1):
+            profile = profile2d.ProjectionY("%s_bin_%i_final"%(profile2d.GetName(),mass),mass+1, mass+1)
+            profile = kde(profile)
+            normalise(profile, density=True)
+            bestOmegaFitted_ = f.Eval(h.GetBinCenter(mass + 1)) + np.finfo(float).eps
+            bestOmegaFitted = max(bestOmegaFitted_, 1E-04)
+            #bestOmegaFitted = h.GetBinContent(mass + 1)
+            print "bestOmegaFitted[{}] = {} --> {}".format(mass, bestOmegaFitted_, bestOmegaFitted)
+            F,Ginv = get_css_fns(bestShapeVal, bestOmegaFitted, profile, "")
 
             # Save classifier
             saveclf(F,    'models/css/css_%s_F_%i.pkl.gz' % (var,mass))
@@ -115,15 +175,17 @@ def perform_optimisation (var, bins, data):
         c = rp.canvas(batch=True)
 
         # -- Plots
-        c.hist(bestOmegas, bins=MASS_BINS, linecolor=rp.colours[1])
+        #c.hist(bestOmegas, bins=MASS_BINS, linecolor=rp.colours[1])
+        c.hist(h, linecolor=rp.colours[1], option='HIST', label="Measured")
+        f.Draw('SAME')
 
         # -- Decorations
         c.xlabel("Large-#it{R} jet mass [GeV]")
         c.ylabel("Best-fit #Omega_{D}")
-        c.text(["#sqrt{s} = 13 TeV,  QCD jets",
-                "CSS applied to D_{2}",
-                "Best-fit #alpha = {:.0f}".format(bestShapeVal)], qualifier=QUALIFIER)
-
+        c.text(["#sqrt{s} = 13 TeV,  Multijets",
+                "CSS applied to {}".format(latex(var, ROOT=True)),
+                "Best-fit #alpha = {:.1f}".format(bestShapeVal)], qualifier=QUALIFIER)
+        c.legend(categories=[('Functional fit', {'linewidth': 2, 'linestyle': 2, 'linecolor': rp.colours[4]})])
         # Save
         c.save('figures/css/cssBestOmega_{}.pdf'.format(var))
         pass
@@ -144,14 +206,13 @@ def fit (profile, shapeVal, lowMassProfile, name):
     bestOmega = 0.01
     bestChi2 = 1e30
     rebinLowMass = lowMassProfile.Clone("%s_%.2f_clone"%(name, shapeVal))
-    rebinLowMass.Rebin(REBIN)
-    normalise(rebinLowMass)
+    normalise(rebinLowMass, density=True)
 
     # Find optimal value for omega for this mass bin
     for omega in OMEGA_RANGE:
         jssVar_css = get_css(shapeVal, omega, profile, "_%.2f"%(omega))
-        jssVar_css.Rebin(REBIN)
-        normalise(jssVar_css)
+        jssVar_css = kde(jssVar_css)
+        normalise(jssVar_css, density=True)
 
         chi2 = 0
         for i in range(1, jssVar_css.GetNbinsX()+1):
@@ -170,8 +231,8 @@ def fit (profile, shapeVal, lowMassProfile, name):
 
     # Compute CSS-transformed version of `var`
     jssVar_css = get_css(shapeVal, bestOmega, profile, "_%.2f_best"%(bestOmega))
-    jssVar_css.Rebin(REBIN)
-    normalise(jssVar_css)
+    jssVar_css = kde(jssVar_css)
+    normalise(jssVar_css, density=True)
 
     return bestChi2, bestOmega, jssVar_css, rebinLowMass
 
