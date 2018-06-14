0dd6d0759bce175571f0a26451a43f541edbe4d1
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-06-14 10:27:19 +0100

    Update uBoost test script

--- run/uboost/test.py
+++ run/uboost/test.py
@@ -25,32 +25,19 @@ from adversarial.profile import profile, Profile
 @profile
 def main (args):
 
-    # Initialising
-    # --------------------------------------------------------------------------
+    # Initialise
     args, cfg = initialise(args)
 
+    # Load data
+    data, features, _ = load_data(args.input + 'data.h5', sample=0.01)  # @TEMP
 
-    # Loading data
-    # --------------------------------------------------------------------------
-    data, features, _ = load_data(args.input + 'data_1M_10M.h5')
-    data = data.sample(frac=0.5, random_state=32)   # @TEMP
-
+    # Define classifier configuration(s)
+    pattern = 'uboost_ur_{:4.2f}_te_92_rel21_fixed_def_cfg_1000boost'
+    urs = [0.0, 0.01, 0.1, 0.3]
+    classifiers = [('AdaBoost' if ur == 0 else 'uBoost (#alpha={:4.2f})'.format(ur), pattern.format(ur).replace('.', 'p')) for ur in urs]
 
     # Test classifiers in parallel
-    # --------------------------------------------------------------------------
-    classifiers = [
-        #('Adaboost', 'adaboost'),
-        ('uBoost (ur= 0.0, te=0.92)', 'uboost_ur_0p00_te_92_rel21_fixed'),
-        ('uBoost (ur= 0.01, te=0.92)', 'uboost_ur_0p01_te_92_rel21_fixed'),
-        ('uBoost (ur= 0.1, te=0.92)', 'uboost_ur_0p10_te_92_rel21_fixed'),
-        ('uBoost (ur= 0.3, te=0.92)', 'uboost_ur_0p30_te_92_rel21_fixed'),
-        #('uBoost (ur= 0.5, te=0.92)', 'uboost_ur_0p50_te_92'),
-        #('uBoost (ur= 1.0, te=0.92)', 'uboost_ur_1p00_te_92'),
-        #('uBoost (ur=3.0, te=0.92)', 'uboost_ur_3p00_te_92'),
-    ]
-
     njobs = min(7, len(classifiers))
-
     with Profile("Run tests in parallel"):
         Parallel(n_jobs=njobs)(delayed(test)(data, title, name) for title, name in classifiers)
         pass
@@ -73,35 +60,12 @@ def test (data, title, name):
                 pass
             pass
 
-
-        # Feature importances
-        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        #### with Profile("Feature importances"):
-        ####     # @NOTE: From scikit-learn (http://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html#sklearn.tree.DecisionTreeClassifier.feature_importances_)
-        ####     #          "The importance of a feature is computed as the
-        ####     #           (normalized) total reduction of the criterion
-        ####     #           brought by that feature. It is also known as the
-        ####     #           Gini importance."
-        ####     for feature, importance in zip(features, clf.feature_importances_):
-        ####         print "{:12s}: {:.4f}".format(feature, importance)
-        ####         pass
-        ####     pass
-
-
         # Adding classifier variable
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         with Profile("Adding classifier variable"):
             data[name] = pd.Series(clf.predict_proba(data)[:,1], index=data.index)
-            x = data[name]    .as_matrix().flatten()
-            w = data['weight_adv'].as_matrix().flatten()
-
-            # Rescale to have weighted mean and RMS of 1/2 and 1/3, resp.
-            N = float(x.size)
-            m = np.sum(x * w) / np.sum(w)
-            s = np.sqrt(np.sum(w * np.square(x - m)) / ((N - 1) * np.sum(w) / N))
-            data[name] -= m
-            data[name] /= s * 3.
-            data[name] += 0.5
+            x = data[name]        .values
+            w = data['weight_adv'].values
             pass
 
 
@@ -109,40 +73,26 @@ def test (data, title, name):
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         with Profile("Plotting learning process"):
 
-	    print("name:",name)
+            print("name:",name)
+
+            # Get training/test split masks
+            msk_train = data['train'] == 1
+            msk_test  = data['train'] == 0
 
             # Get predictions
-            pred_train = clf.predict_proba(data[data['train'] == 1])[:,1]
-            pred_test  = clf.predict_proba(data[data['train'] == 0])[:,1]
+            pred_train = clf.predict_proba(data[msk_train])[:,1].flatten()
+            pred_test  = clf.predict_proba(data[msk_test]) [:,1].flatten()
 
             # Get staged predicting, i.e. for each successive estimator
-            idx_train = np.where(data['train'] == 1)[0]
-            idx_test  = np.where(data['train'] == 0)[0]
+            staged_pred_train = list(clf.staged_predict_proba(data[msk_train]))
+            staged_pred_test  = list(clf.staged_predict_proba(data[msk_test]))
 
+            y_train = data.loc[msk_train, 'signal']    .values.flatten()
+            y_test  = data.loc[msk_test,  'signal']    .values.flatten()
+            w_train = data.loc[msk_train, 'weight_adv'].values.flatten()
+            w_test  = data.loc[msk_test,  'weight_adv'].values.flatten()
 
-            idx_train = np.random.choice(idx_train, int(0.05 * idx_train.size), replace=False)
-            idx_test  = np.random.choice(idx_test , int(0.05 * idx_test .size), replace=False)
-
-            #staged_pred_train = list(clf.staged_predict_proba(data.loc[idx_train]))
-            #staged_pred_test  = list(clf.staged_predict_proba(data.loc[idx_test]))
-            
-	    staged_pred_train = list(clf.staged_predict_proba(data.loc[data.index.get_values()[idx_train]]))
-            staged_pred_test  = list(clf.staged_predict_proba(data.loc[data.index.get_values()[idx_test]]))
-
-            y_train = data.loc[data.index.get_values()[idx_train],'signal']
-            y_test  = data.loc[data.index.get_values()[idx_test],'signal']
-            w_train = data.loc[data.index.get_values()[idx_train],'weight_adv']
-	    w_test = data.loc[data.index.get_values()[idx_test],'weight_test']
-
-            #staged_pred_train = list(clf.staged_predict_proba(data[data['train'] == 1]))
-            #staged_pred_test  = list(clf.staged_predict_proba(data[data['train'] == 0]))
-
-            #y_train = data.loc[data['train'] == 1, 'signal'].values
-            #y_test  = data.loc[data['train'] == 0, 'signal'].values
-            #w_train = data.loc[data['train'] == 1, 'weight_adv'].values
-            #w_test  = data.loc[data['train'] == 0, 'weight_adv'].values
-
-            ll_train, ll_test = list(), list()
+            ll_train,  ll_test  = list(), list()
             auc_train, auc_test = list(), list()
 
             for idx, (pred_train, pred_test) in enumerate(zip(staged_pred_train, staged_pred_test)):
@@ -183,7 +133,7 @@ def test (data, title, name):
             plt.savefig('figures/temp_auc_{}.pdf'.format(name))
             pass
 
-
+        '''
 
         # Plotting ROC curves
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -253,7 +203,7 @@ def test (data, title, name):
                     # Cast
                     num   = num  .astype(np.float)
                     denom = denom.astype(np.float)
-		
+
 		    #print("mass: ",data.loc[msk_bkg,'m'].head(5))
 		    #print("weight: ",data.loc[msk_bkg,'weight_test'].head(5))
 		    #print("histo: ",denom)
@@ -271,6 +221,7 @@ def test (data, title, name):
             mkdir('figures/')
             plt.savefig('figures/temp_eff_{}.pdf'.format(name))
             pass
+        #'''
         pass
     return
 
