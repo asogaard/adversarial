3e03dda06a9613e968bcf5908b34239b40b424f5
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-06-15 11:03:59 +0100

    Update BDT training loss plotting script

--- tests/plot_loss_adaboost_Fig5.py
+++ tests/plot_loss_adaboost_Fig5.py
@@ -1,78 +1,78 @@
 #!/usr/bin/env python
 
+# Basic import(s)
+import pickle
 import numpy as np
 rng = np.random.RandomState(21)  # For reproducibility
-import pandas as pd
 
-import matplotlib.pyplot as plt
+# Project import(s)
 import rootplotting as rp
 
-from ROOT import TGraphErrors
 
-import gzip
-import pickle
-
-
-
-def OpenFile (fi,opt):
-
+# Utility method(s)
+def OpenFile (fi, opt='rb'):
+    """
+    Return pickled file contents.
+    """
     with open(fi, opt) as f:
         x = pickle.load(f)
         pass
 
     return x
 
+
 # Main function definition
 def main ():
 
+    # Global defintions
+    alpha = 0.1
+
+    # Load epochs and log-losses from files
+    pattern = 'tests/inputs_Fig5/{v:s}_uboost_ur_{a:4.2f}_te_92_rel21_fixed_FIG5'
+    x           = OpenFile(pattern.format(v='x_axis', a=alpha).replace('.', 'p') + '.pkl.gz')
+    ll_ab_train = OpenFile(pattern.format(v='train',  a=0)    .replace('.', 'p') + '.pkl.gz')
+    ll_ab_test  = OpenFile(pattern.format(v='test',   a=0)    .replace('.', 'p') + '.pkl.gz')
+    ll_ub_train = OpenFile(pattern.format(v='train',  a=alpha).replace('.', 'p') + '.pkl.gz')
+    ll_ub_test  = OpenFile(pattern.format(v='test',   a=alpha).replace('.', 'p') + '.pkl.gz')
+
+    # Plot log-loss curves
     c = rp.canvas(batch=True)	
+    
+    # -- Common plotting options
+    opts = dict(linewidth=2, legend_option='L')
+    c.graph(ll_ab_train, bins=x, linecolor=rp.colours[5], linestyle=1, option='AL', label='AdaBoost', **opts)
+    c.graph(ll_ab_test,  bins=x, linecolor=rp.colours[5], linestyle=2, option='L',  **opts)
+    c.graph(ll_ub_train, bins=x, linecolor=rp.colours[1], linestyle=1, option='L',  label='uBoost', **opts)
+    c.graph(ll_ub_test,  bins=x, linecolor=rp.colours[1], linestyle=2, option='L',  **opts)
 
-    #inputs needed
-    x = OpenFile('inputs_Fig5/x_axis_uboost_ur_0p10_te_92_rel21_fixed_FIG5.pkl.gz', 'rb')
-    ll_train = OpenFile('inputs_Fig5/train_uboost_ur_0p00_te_92_rel21_fixed_FIG5.pkl.gz', 'rb')
-    ll_test = OpenFile('inputs_Fig5/test_uboost_ur_0p00_te_92_rel21_fixed_FIG5.pkl.gz', 'rb')
-    ll_train_uboost = OpenFile('inputs_Fig5/train_uboost_ur_0p10_te_92_rel21_fixed_FIG5.pkl.gz', 'rb')
-    ll_test_uboost = OpenFile('inputs_Fig5/test_uboost_ur_0p10_te_92_rel21_fixed_FIG5.pkl.gz', 'rb')
-
-
-    bins = np.arange(len(x), dtype=np.float) + 1
-    means_train = np.array(ll_train)
-    means_test = np.array(ll_test)
-    means_train_ub = np.array(ll_train_uboost)
-    means_test_ub = np.array(ll_test_uboost)
-
-    graph1 = TGraphErrors(len(bins), bins, means_train, bins * 0)
-    graph2 = TGraphErrors(len(bins), bins, means_test, bins * 0)
-    graph3 = TGraphErrors(len(bins), bins, means_train_ub, bins * 0)
-    graph4 = TGraphErrors(len(bins), bins, means_test_ub, bins * 0)
-
-    c.graph(graph1, markercolor=rp.colours[4], linecolor=rp.colours[5],linewidth=2, markersize=0.5, linestyle =1,option='AL', label='Train Adaboost ', legend_option='l')
-    c.graph(graph2, markercolor=rp.colours[4], linecolor=rp.colours[5],linewidth=2, markersize=0.5, linestyle =2, option='L', label='Test Adaboost', legend_option='l')
-    c.graph(graph3, markercolor=rp.colours[1], markerstyle=22, markersize=0.5, linecolor=rp.colours[1],linewidth=2,linestyle =1, option='L', label='Train uBoost (#alpha=0.1)', legend_option='l')
-    c.graph(graph4, markercolor=rp.colours[1], markerstyle=22, markersize=0.5, linecolor=rp.colours[1], linewidth=2,linestyle =2, option='L', label='Test uBoost (#alpha=0.1)', legend_option='l')
-
-    ymax = 0.8
-    ymin = 0.3
-    # Decorations
+    # -- Decorations
     c.pad()._yaxis().SetNdivisions(505)
-    c.xlabel("Boosting step / no. estimator")
-    c.ylabel("Log-loss")
-    c.xlim(-10, len(bins))
-    c.ylim(ymin, ymax)
-    c.legend(xmin=.6,xmax=.7,ymin=0.6,ymax=0.8, width=0.22)
-    c.text(["#sqrt{s} = 13 TeV", "#it{W} jet tagging"], qualifier="Simulation Internal")
-    # Save
-    c.save('./uboost_vs_adaboost_logloss_fig5.pdf')
+    c.xlabel("Training epoch")
+    c.ylabel("BDT classifier loss")
+    c.xlim(0, len(x))
+    c.ylim(0.3, 1.4) 
+    c.legend(width=0.28)
+    c.legend(header='Dataset:',
+             categories=[('Training', {'linestyle': 1}),
+                         ('Testing',  {'linestyle': 2})],
+             width=0.28, ymax=0.69)
 
-
+    for leg in c.pad()._legends:
+        leg.SetFillStyle(0)
+        pass
+    
+    c.text(["#sqrt{s} = 13 TeV",
+            "#it{W} jet tagging",
+            "Uniforming rate #alpha = {:3.1f}".format(alpha)],
+           qualifier="Simulation Internal")
+    
+    # -- Save
+    c.save('figures/test_uboost_vs_adaboost_logloss_fig5_alpha{:4.2f}'.format(alpha).replace('.', 'p') + '.pdf')
     return
 
 
 # Main function call
 if __name__ == '__main__':
-
-    # Parse command-line arguments
-    #args = parse_args()
 
     # Call main function
     main()
