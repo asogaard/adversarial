d95599920f30e1bad2ed6c8c3c67e2f19154cd9d
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-11-14 15:56:11 +0000

    Add optimisation plot for combined model

--- tests/optimisation.py
+++ tests/optimisation.py
@@ -28,10 +28,10 @@ def main (args):
     args, cfg = initialise(args)
 
     # Common definitions
-    experiment  = 'classifier'
+    experiment  = 'combined'  # 'classifier'
     paths = sorted(glob.glob('optimisation/output/{}/output/*.out'.format(experiment)))
     
-    num_steps = 100
+    num_steps = 100 if experiment == 'classifier' else 200
 
     # Loop all run outputs
     means, stds, results = list(), list(), list()
@@ -44,23 +44,38 @@ def main (args):
             # Number of training epochs, to identify the last one
             num_epochs = max(map(int, map(lambda l: l.split('/')[-1], filter(lambda l: re.search('^Epoch [\d]+/[\d]+ *$', l), lines))))
 
-            # Indices of line holding the results for the last training epoch in each CV fold
-            try:
-                indices = np.array(zip(*filter(lambda t: 'Epoch {e}/{e}'.format(e=num_epochs) in t[1], enumerate(lines)))[0]) + 1
-            except IndexError: continue
+            if experiment == 'classifier':
+                # Indices of line holding the results for the last training epoch in each CV fold
+                try:
+                    indices = np.array(zip(*filter(lambda t: 'Epoch {e}/{e}'.format(e=num_epochs) in t[1], enumerate(lines)))[0]) + 1
+                except IndexError: continue
+                
+                # Validation losses for each CV fold
+                val_losses = list()
+                for idx in indices:
+                    fields = lines[idx].split()
+                    jdx = fields.index('val_loss:') + 1
+                    val_loss = float(fields[jdx])
+                    val_losses.append(val_loss)
+                    pass
+                
+                # Append results for current evaluation
+                if any(np.isnan(val_losses)):
+                    continue
+                means.append(np.mean(val_losses))
+                stds .append(np.std (val_losses))
+            else:
+                try:
+                    line = filter(lambda line: 'rej + 1/jsd' in line, lines)[0]
+                except IndexError:
+                    line = ': 0.0 ± 0.0'
+                    pass
 
-            # Validation losses for each CV fold
-            val_losses = list()
-            for idx in indices:
-                fields = lines[idx].split()
-                jdx = fields.index('val_loss:') + 1
-                val_loss = float(fields[jdx])
-                val_losses.append(val_loss)
+                # Extract mean +/- std for combined optimisation metric
+                mean_std = map(float,line.split(':')[-1].strip().split("±"))
+                means.append(mean_std[0])
+                stds .append(mean_std[1])
                 pass
-
-            # Append results for current evaluation
-            means.append(np.mean(val_losses))
-            stds .append(np.std (val_losses))
             pass
         pass
 
@@ -74,7 +89,11 @@ def main (args):
     means = np.array(means)
     stds  = np.array(stds)
     bins = np.arange(len(means), dtype=np.float) + 1
-    best_mean = np.array([np.min(means[:i+1]) for i in range(len(means))])
+    if experiment == 'classifier':
+        best_mean = np.array([np.min(means[:i+1]) for i in range(len(means))])
+    else:
+        best_mean = np.array([np.max(means[:i+1]) for i in range(len(means))])
+        pass
     idx_improvements = [0] + list(np.where(np.abs(np.diff(best_mean)) > 0)[0] + 1)
 
     # Create graph
@@ -96,31 +115,36 @@ def plot (*argv):
 
     # Plot results
     c = rp.canvas(batch=True)
-    ymax = 1.0 # 1.5
-    ymin = 0.3
+    if experiment == 'classifier':
+        ymax = 1.0 # 1.5
+        ymin = 0.3
+    else:
+        ymax = 1500.0
+        ymin = 0.0
+        pass
     oobx = map(lambda t: t[0], filter(lambda t: t[1] > ymax, enumerate(means)))
     ooby = np.ones_like(oobx) * 0.96 * (ymax - ymin) + ymin
 
     # Plots
     markersize = 0.8
     c.graph(graph, markercolor=rp.colours[1], linecolor=rp.colours[1], markerstyle=20, markersize=markersize, option='AP', label='Evaluations', legend_option='PE')
-    c.graph(ooby, bins=oobx, markercolor=rp.colours[1], markerstyle=22, option='P')
+    if len(ooby):
+        c.graph(ooby, bins=oobx, markercolor=rp.colours[1], markerstyle=22, option='P')
+        pass
     c.graph(best_mean, bins=bins, linecolor=rp.colours[5], linewidth=2, option='L')
     c.graph(best_mean[idx_improvements], bins=bins[idx_improvements], markercolor=rp.colours[5], markerstyle=24, markersize=markersize, option='P')
 
     # Decorations
     c.pad()._yaxis().SetNdivisions(505)
     c.xlabel("Bayesian optimisation step")
-    c.ylabel("Cross-validation optimisation metric, L_{clf}^{val}")
+    c.ylabel("Cross-val. optimisation metric, " + ("L_{clf}^{val}" if experiment == 'classifier' else '1/#varepsilon_{bkg}^{rel} + #lambda/JSD'))
     c.xlim(0, len(bins))
-    #c.ylim(0, ymax)
-    c.ylim(0.3, 1.0)
+    c.ylim(ymin, ymax)
     c.legend(width=0.22, ymax=0.816, categories=[
             ('Best result', dict(linecolor=rp.colours[5], linewidth=2, markercolor=rp.colours[5], markerstyle=24, option='LP')),
         ])
-    c.text(["#sqrt{s} = 13 TeV",
-            "Neural network (NN) classifier"
-            ],
+    c.text(["#sqrt{s} = 13 TeV"] + \
+          (["Neural network (NN) classifier"] if experiment == 'classifier' else ["Adversarial neural network (ANN)", "classifier"]),
            qualifier=QUALIFIER)
     # Save
     mkdir('figures/optimisation/')
